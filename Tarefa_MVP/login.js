8e5036b234e0737a6c1764a1da45a87a79015872
KaikyAlencar <111911949+KaikyHanry@users.noreply.github.com>
2025-11-10 09:49:27 -0300

    Correcoes

--- Tarefa_MVP/login.js
+++ Tarefa_MVP/login.js
@@ -8,14 +8,11 @@ document.addEventListener("DOMContentLoaded", function () {
         const usuario = document.querySelector("input[type='email']").value.trim();
         const senha = document.querySelector("input[type='password']").value.trim();
 
-        // Verificar se os campos estão preenchidos
         if (usuario === "" || senha === "") {
             alert("Por favor, preencha todos os campos.");
             return;
         }
 
-        // Aqui você poderia validar o usuário e senha
-        // Por enquanto, só redireciona
         alert("Login aceito!");
         window.location.href = "agendar.html";
     });
